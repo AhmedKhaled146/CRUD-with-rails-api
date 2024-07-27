ea9db518e71b26f63c03fa26a86ea59ffd8058e6
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-07-27 15:05:55 +0300

    add auth before add action on CRUD items

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
   before_action :set_todo, only: [:edit, :destroy, :update, :show]
 
   def index
